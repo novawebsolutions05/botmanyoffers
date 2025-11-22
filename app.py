4890411469695f412832fad1ba4599257a952251
novawebsolutions05 <novawebsolutions05@gmail.com>
2025-11-22 15:06:55 -0500

    Usar puerto dinámico para Render

--- app.py
+++ app.py
@@ -222,8 +222,10 @@ if __name__ == "__main__":
     
     print("Rutas registradas en Flask:")
     print(app.url_map)
-
-    app.run(host="0.0.0.0", port=5001)
+    
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
